0515f310b4cc69a91a687650619a2c3da061dbf6
siko408 <gamerbloxhd@gmail.com>
2019-11-19 18:54:55 -0800

    Adding 8-Ball

---
+++ Donate_version1/Donate_version1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Donate_version1
+//
+//  Created by Jose Arellanes on 11/18/19.
+//  Copyright © 2019 Jose Arellanes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Donate_version1/Donate_version1/MyImpactView/Alerts.swift
@@ -0,0 +1,16 @@
+//
+//  Alerts.swift
+//  Donate_version1
+//
+//  Created by Jose Arellanes on 11/18/19.
+//  Copyright © 2019 Jose Arellanes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Alerts: UIViewController{
+/// Practice connecting the buttons
+    
+}
